fb8075d832c3209573fa12cd04a5af5e52dd7ef5
Keith Holmes <keithholmes248@gmail.com>
2016-04-26 10:04:09 -0500

    initial py files

---
+++ gpio-ws.py
@@ -0,0 +1,39 @@
+import sys
+import json
+import time
+
+from ws4py.client.threadedclient import WebSocketClient
+
+
+
+
+####### Waterproof Temperature Sensor   ########
+# https://github.com/timofurrer/w1thermsensor
+# from w1thermsensor import W1ThermSensor
+
+# sensor = W1ThermSensor()
+# temperature_in_celsius = sensor.get_temperature()
+# temperature_in_fahrenheit = sensor.get_temperature(W1ThermSensor.DEGREES_F)
+# temperature_in_all_units = sensor.get_temperatures([
+#     W1ThermSensor.DEGREES_C,
+#     W1ThermSensor.DEGREES_F,
+#     W1ThermSensor.KELVIN])
+####### Waterproof Temperature Sensor   ########
+
+
+# https://github.com/seanbechhofer/raspberrypi/blob/master/python/TSL2561.py
+
+# https://github.com/adafruit/Adafruit_Python_DHT
+
+class wsClient(WebSocketClient):
+    def opened(self):
+        self.send("I Have Connected!!!")    
+
+
+if __name__ == '__main__':
+    try:
+        ws = wsClient('ws://localhost:8000/')
+        ws.connect()
+        ws.run_forever()
+    except KeyboardInterrupt:
+        ws.close()
